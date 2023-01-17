05a1a5052e4be562db396b51251d95f225fb6972
SebastianKru <39736641+SebastianKru@users.noreply.github.com>
2023-01-18 10:20:54 +1300

    finished first half of tasks. found matching functions and biggest delta
    - added classes
    - added inheritance
    
    TODO: split monolyth in multiple files

--- input-handler.py
+++ input-handler.py
@@ -9,10 +9,18 @@ import time
 import sqlalchemy
 import pymysql 
 import sqlalchemy as db 
+#import sqlalchemy
 from alembic import op
 from alembic.migration import MigrationContext
 from alembic.operations import Operations
 from sqlalchemy import inspect
+from sqlalchemy import func
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import text, column
+
+#Fehler abfangen: filePath does not exist 
+#Fehler abfangen: anzahl der zeilen != 40 
+#Objektorientierung. Klasse mit x und y wert anlegen und vererben zu klasse training data, test data und ideal data klasse 
 
 # declare strings of csv file names
 train_csv_name = "train.csv"
@@ -26,7 +34,6 @@ ideal_table_name = "IdealFunctions"
 test_table_name = "TestingData"
 
 train_table = ideal_table = test_table = None
-
 
 
 # get the csv file paths. csv files are in the folder "input-data"
@@ -51,29 +58,101 @@ def createTablesFromDF(table_name, pandas_df, eng, meta_d):
     table = db.Table(table_name, meta_d, autoload=True, autoload_with = eng)
     return table
 
-# loop over a sql table and change the column headings 
+# loop over the pandas dataframe and change the column headings 
 # e.g. from lowercase x to uppercase X and lowercase y to uppercase Y
-def alterColumnNames(engine, table_name, columns, x, x_new, y, y_new, y_label):
-    alterOneColumnName(engine, table_name, x, x_new)
+def alterColumnNames(table_df, x, x_new, y, y_new, y_label):
+    number_of_cols = len(table_df.axes[1])
     i = 1
-    while i < columns: 
-       alterOneColumnName(engine, table_name, y + str(i), y_new + str(i) + y_label)
+
+    table_df.rename(columns = {x:x_new}, inplace = True)
+    while i < number_of_cols: 
+       table_df.rename(columns = {y + str(i):y_new + str(i) + y_label}, inplace = True)
        i+=1
 
-# helper function for 'alterColumntNames' 
-# using alembic library to change the heading of a column 
-def alterOneColumnName(engine, table, name, name_new):
-    # Create migration context
-    mc = MigrationContext.configure(engine.connect())
-    # Creation operations object
-    ops = Operations(mc)
-    ops.alter_column(table,column_name = name,new_column_name = name_new,existing_type = db.String(100))
+#def getColumnByID(table, columnID, connection):
+#    selector = db.select([table.columns[columnID]])
+#    column = connection.execute(selector).fetchall()
+#    return column
+
+def sqlToArray(session, table, nr_of_columns):
+    sqlTable = session.query(table).all()
+    two_dim_array = []
+    i = 0
+    while i < nr_of_columns:
+        col = []
+        for column in sqlTable:
+            col.append(column[i])
+        two_dim_array.append(col)
+        i+=1
+    return two_dim_array
+
+
+class BaseFunction(object): 
+    def __init__(self, x, y):
+        self.x_values = x
+        self.y_values = y
+
+class TrainingFunction(BaseFunction): 
+    name = ""
+    ideal_function = None
+    mse = None
+    max_delta = None  
+    length = None
+
+    def __init__(self, x, y, name):
+        super().__init__(x, y)
+        self.name = name
+        self.length = len(self.x_values)
+
+class TestFunction(BaseFunction): 
+    pass
+
+def calculateSmallestMSE(tf, ideal_array):
+    columns_ideal_array = len(ideal_array) 
+    col = 1
+    mse_array = []
+
+    while col < columns_ideal_array:
+        squared_error = 0
+        row = 0
+        for value in range(0, tf.length): 
+            squared_error += (tf.y_values[row] - ideal_array[col][row]) ** 2
+            row += 1
+        mse_array.append(squared_error / tf.length)
+        #print('mse of ', col, ' = ', mse_array[col-1])
+        col += 1
+
+    tf.mse = mse_array[0]
+    for i in range(0, len(mse_array)):
+        if mse_array[i] < tf.mse:
+            tf.mse = mse_array[i]
+            tf.ideal_function = i
+
+    #ideal function, loop through rows to find biggest deviation 
+    deltas = []
+    row = 0
+    for value in range(0, tf.length): 
+       deltas.append(tf.y_values[row] - ideal_array[tf.ideal_function ][row])
+       row += 1
+    
+    tf.max_delta = deltas[0]
+    for i in range(0, len(deltas)):
+        if deltas[i] > tf.max_delta: 
+            tf.max_delta = deltas[i]
+
+    print ('the smallest MSE for training function', tf.name, ' is: ', 
+    tf.mse, ' in ideal function: ', tf.ideal_function)
+    print('the biggest delta is: ', tf.max_delta)
+
 
 def main(): 
     engine = db.create_engine("mysql+pymysql://root:password@localhost/ui-sem1-python-assignment")
     connection = engine.connect()
     meta_data = db.MetaData()
+
     meta_data.create_all(engine)
+    Session = sessionmaker(bind = engine)
+    session = Session()
 
     train_file_path = getCSVFilePath(train_csv_name)
     test_file_path = getCSVFilePath(test_csv_name)
@@ -83,13 +162,28 @@ def main():
     ideal_table_df = createPandasDF(ideal_file_path)
     test_table_df = createPandasDF(test_file_path)
 
+    alterColumnNames(train_table_df, 'x','X','y','Y', '_training_func')
+    alterColumnNames(ideal_table_df, 'x','X','y','Y', '(ideal-func)')
+
     train_table = createTablesFromDF(train_table_name, train_table_df, engine, meta_data)
     ideal_table = createTablesFromDF(ideal_table_name, ideal_table_df, engine, meta_data)
     test_table = createTablesFromDF(test_table_name, test_table_df, engine, meta_data)
 
-    alterColumnNames(engine, train_table_name, train_table_df.shape[1], 'x','X','y','Y', ' (training func)')
-    alterColumnNames(engine, ideal_table_name, ideal_table_df.shape[1], 'x','X','y','Y', ' (ideal func)')
+    train_array = sqlToArray(session, train_table, len(train_table_df.axes[1]))
+    ideal_array = sqlToArray(session, ideal_table, len(ideal_table_df.axes[1]))
+
+    tf1 = TrainingFunction(train_array[0], train_array[1], "Training Function 1")
+    tf2 = TrainingFunction(train_array[0], train_array[2], "Training Function 2")
+    tf3 = TrainingFunction(train_array[0], train_array[3], "Training Function 3")
+    tf4 = TrainingFunction(train_array[0], train_array[4], "Training Function 4")
+
+    calculateSmallestMSE(tf1, ideal_array)
+    calculateSmallestMSE(tf2, ideal_array)
+    calculateSmallestMSE(tf3, ideal_array)
+    calculateSmallestMSE(tf4, ideal_array)
+
 
 
 if __name__ == '__main__':
     main()
+
